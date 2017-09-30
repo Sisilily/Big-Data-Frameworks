9523aeccb95494b315eabf390d1385724a18facf
Sisilily <yu.li.telecompt@gmail.com>
2017-09-30 22:12:02 +0200

    Update echo.java

--- echo.java
+++ echo.java
@@ -3,12 +3,12 @@ public class echo {
         // write your code here
         if(args.length==0) {
 
-        }
+        } //if no argument is given, then do nothing
         else {
             for(int i=0;i<args.length-1;i++) {
                 System.out.print(args[args.length-1-i] +" ");        }
             System.out.println(args[0]);
-        }
+        } 
 
 
     }
